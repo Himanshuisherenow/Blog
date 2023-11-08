64de99f7b355c4fbc902ec928a983e03c918d45e
Chavda Himanshu H <himanshuisherenow@gmail.com>
2023-11-08 16:29:28 +0530

    solved proxy server and cors problem with using vite config js and also learned about build and dist in corporate

--- Documents/GitHub/COCBack/backend/server.js
+++ Documents/GitHub/COCBack/backend/server.js
@@ -1,33 +1,36 @@
+const { configDotenv } = require('dotenv')
 const express = require('express')
+configDotenv()
 const app = express()
 const PORT = process.env.PORT||3000 
 
-app.get('/api',(req,res)=>{
+// app.use(express.static('dist')) small company problem
+
+app.get('/api ',(req,res)=>{
     res.send('<h1>Hello world</h1>')
       
 })
 
 app.get('/api/jokes',(req,res)=>{
-    res.json({
-        "jokes": [
-          {
-            "id": 1,
-            "title": "The Chicken Crossing",
-            "content": "Why did the chicken cross the road? To get to the other side!"
-          },
-          {
-            "id": 2,
-            "title": "Dad Joke Alert",
-            "content": "Did you hear about the cheese factory that exploded? There was nothing left but de-brie."
-          },
-          {
-            "id": 3,
-            "title": "Pun-tastic",
-            "content": "I used to play piano by ear, but now I use my hands."
-          }
-        ]
-      }
-      )
+
+  const jokes = [
+    {
+      "id": 1,
+      "title": "The Chicken Crossing",
+      "content": "Why did the chicken cross the road? To get to the other side!"
+    },
+    {
+      "id": 2,
+      "title": "Dad Joke Alert",
+      "content": "Did you hear about the cheese factory that exploded? There was nothing left but de-brie."
+    },
+    {
+      "id": 3,
+      "title": "Pun-tastic",
+      "content": "I used to play piano by ear, but now I use my hands."
+    }
+  ]
+    res.send(jokes)
 })
  
 app.listen(PORT,()=>{
