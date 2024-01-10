d81a33667a1f47c59cee74efbd4c8c61327b8333
Chavda Himanshu H <himanshuisherenow@gmail.com>
2024-01-10 17:37:02 +0530

    side bar and svgs and links of social sites

---
+++ Documents/GitHub/todo/todo/src/App.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import Appp from './component/Appp';
+import Password from './Password';
+
+function App() {
+
+  
+
+  return (
+    <>
+    <Appp/>
+    </>
+  )
+}
+
+export default App
