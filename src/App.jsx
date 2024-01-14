f457ac08c39feee09047056ded70cf2885ee547c
Chavda Himanshu H <himanshuisherenow@gmail.com>
2024-01-15 01:31:21 +0530

    reduxtoolkit and store configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login } from './store/authSlice'
+import { Header ,Footer} from './components'
+import { Outlet } from 'react-router'
 
 function App() {
   
+  const [loading,setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-     
-    </>
-  )
+  useEffect(()=>{
+
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=>setLoading(false))
+  },[])
+
+  return loading? null :  <div className='min-h-screen flex-wrap content-between bg-gray-600'>
+    <div className='w-full block'>
+      <Header/>
+      <main>
+        <Outlet/>
+      </main>
+      <Footer/>
+    </div>
+  </div>
 }
 
 export default App
