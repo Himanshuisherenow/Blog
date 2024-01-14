e69101329a140f36f57fb3c0507b2d0c90e75750
Chavda Himanshu H <himanshuisherenow@gmail.com>
2024-01-15 01:07:20 +0530

    store and slice is created

---
+++ src/store/authSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+const initialState = {
+    
+    status : false,
+    userData : null
+}
+
+
+const authSlice = createSlice({
+
+    name : "auth",
+    initialState,
+    reducers : { 
+
+        login : (state,action)=>{
+
+            const {userData} = action.payload
+            state.status = true 
+            state.userData  = userData
+        },
+        logout : (state,action)=>{
+
+
+        }
+    }
+})
+
+export const {login,logout} = authSlice.actions   /// login and logout called as acitons and reducer is obj
+export default authSlice.reducer
